db26e5800c38ad133ac26ee8fb2e1a4ba78336b5
Yoseph Tapano <yosephman76@gmail.com>
2023-03-17 08:28:27 +0300

    main

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,8 +7,8 @@ Base = declarative_base()
 
 
 class User(Base):
-    """represent of a user"""
-    __tablename__ == 'users'
+    """Representation of a user """
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
